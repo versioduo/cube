3337100612ed35d762a80bc72cd0f2744a9a39fc
Kay Sievers <kay@versioduo.com>
2026-06-21 21:53:10 +0200

    Update MIDI API

--- firmware/cube/cube.ino
+++ firmware/cube/cube.ino
@@ -1031,7 +1031,7 @@ private:
   // Receive a host event from our parent device.
   void receivePlug(V2Link::Packet* packet) override {
     if (packet->getType() == V2Link::Packet::Type::MIDI) {
-      packet->receive(&_midi);
+      packet->copyTo(_midi);
       Device.dispatch(&Plug, &_midi);
     }
   }
@@ -1044,7 +1044,7 @@ private:
         return;
 
       if (Device.usb.midi.connected()) {
-        packet->receive(&_midi);
+        packet->copyTo(_midi);
         _midi.setPort(address + 1);
         Device.usb.midi.send(&_midi);
       }
